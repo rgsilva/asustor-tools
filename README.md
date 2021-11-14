66126da0f783e2583a2ed09df87f7fc01d2d37fb
Ricardo <noreply@noreply.com>
2021-11-14 17:55:05 -0300

    And so it begins\! + fan control tool

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# asustor
+
+
+
+# Asustor Tools
+
+This is a repository for storing some of my personal tools for hacking, modding and/or
+reverse engineering Asustor's NAS operating system. Anything you see here is free to use,
+just give me credits :)
+
+# Disclaimer
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+**TL;DR: if something gets on fire, it's your fault. Sorry.**
+
+# The tools
+
+## fan-control
+
+This is a simple tool to provide you a better control of your NAS fans. For some reason
+these devices can get hotter than normal and somehow the OS is not properly controlling
+the fans, so this tool can help with that. Or you just want your NAS to be really quiet,
+or really cold. Anyway, this is the tool for you.
+
+```
+./asustor-fan-control <fans> <sleep> <curve>
+
+fans:  comma-separated fan indexes
+  ex:  0,1,2,3
+sleep: nanoseconds to sleep between each PWM check
+  ex:  10000
+curve: comma-separated temp:pwm ordered by temp
+  ex:  30:0,40:50,50:100,60:150,70:255
+```
+
+It's not a daemon yet, so you need to run this inside a screen or something.
+
+### Missing features
+
+1. This tool does not support NVMe drives.
+2. This tool is getting the available drives by brute-forcing it. We should be able to find them
+   using the NAS functions.
+3. No easy install yet (either a full application or a service).
+4. No configuration file (everything is in the arguments for now).
+
+# How to build the tools?
+
+This is tricky, but I have a Docker container for that which I'll publish a compose
+eventually. Right now you can build the tools by doing the following:
+
+1. Create an Ubuntu 20.04 LTS Docker container, mounting the following folders:
+    *  `/usr/lib` as `/mnt/lib`
+    * `/root/ubuntu` as `/root/nas`
+2. Use the `build.sh` script I provided in this repository. It'll try and link the provided file
+  to anything everything. This should work just fine.
+3. The tool should be available at `/root/ubuntu`.
+
+# FAQ
+
+1. Is this legal?
+  It depends on your location. Since I'm not providing any source code to anything I've been
+working on besides the tools, I believe it's still legal.
+
+2. How do I contact you?
+  You don't. But if you must, GitHub is your friend and I'm also trackable in Twitter.
+
+3. Why this repository?
+  I have way too many tools already from my recent experimentation on reverse engineering
+Asustor's NAS firmware. I actually own one of their products and I quite enjoy it, so
+adding features, even if it requires some hard work, is worth sometimes!
+
+4. Tell me more!
+  Check [my blog](https://blog.rgsilva.com/), I have a bunch of posts playing with my NAS! :)
---
+++ fan-control.c
@@ -0,0 +1,248 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define MAX_CURVE 255
+#define MAX_FANS 10
+#define ARG_DELIM ","
+#define TEMP_DELIM ':'
+#define MAX_DISKS 10
+#define MAX_DISK_NAME 8
+#define CHECK_TEMP_INTERVAL 10
+
+// Fan functions
+
+extern int Hal_Fan_Get_Raw_Value(ulong p1, uint *p2);
+extern int Hal_Fan_Set_Raw_Value(ulong p1, uint p2);
+
+// System and CPU thermal functions
+
+#define SYSTEMP 2
+#define CPUTEMP 3
+
+extern int _Z29Hal_Sensor_Get_Thermal_Status18HAL_EN_SENSOR_TYPERi(int p1, int *p2);
+#define Hal_Sensor_Get_Thermal_Status _Z29Hal_Sensor_Get_Thermal_Status18HAL_EN_SENSOR_TYPERi
+
+// HDD functions
+
+extern int Hal_Disk_Get_Temperature(char *p1, double *p2, char *p3);
+extern int _Z20Get_Disk_Device_NameiiPc(int p1, int p2, char *p3);
+
+// Our magic
+
+typedef struct {
+  int temp;
+  int pwm;
+} CurveItem;
+CurveItem EmptyCurveItem = {-1, -1};
+
+CurveItem curve[MAX_CURVE];
+int fans[MAX_FANS];
+uint sleepTime;
+char disks[MAX_DISK_NAME][MAX_DISKS];
+uint checkTemp;
+
+static volatile int keepRunning = 1;
+
+void signalHandler(int _) {
+  keepRunning = 0;
+}
+
+void help(char *arg0) {
+    printf("%s <fans> <sleep> <curve>\n", arg0);
+    printf("\n");
+    printf("fans:  comma-separated fan indexes\n");
+    printf("  ex:  0,1,2,3\n");
+    printf("sleep: nanoseconds to sleep between each PWM check\n");
+    printf("  ex:  10000\n");
+    printf("curve: comma-separated temp:pwm ordered by temp\n");
+    printf("  ex:  30:0,40:50,50:100,60:150,70:255\n");
+}
+
+void parseFansArg(char *arg) {
+  for (int i = 0; i < MAX_FANS; i++) { fans[i] = -1; }
+
+  int index = 0;
+  char *ptr = strtok(arg, ARG_DELIM);
+  while (ptr != NULL) {
+    if (index < MAX_FANS) {
+      fans[index] = atoi(ptr);
+    } else {
+      printf("WARN: ignoring fan %s: too many fans!\n", ptr);
+    }
+    ptr = strtok(NULL, ARG_DELIM);
+    index++;
+  }
+
+  printf("Fans: ");
+  for (int i = 0; i < MAX_FANS; i++) {
+    if (fans[i] != -1) {
+      printf("%d ", fans[i]);
+    }
+  }
+  printf("\n");
+}
+
+void parseSleepArg(char *arg) {
+  sleepTime = atoi(arg);
+  printf("Sleep: %u ns\n", sleepTime);
+}
+
+CurveItem parseCurveItem(char *arg) {
+  char temp[32] = {0};
+  char pwm[32] = {0};
+
+  char *pwmPointer = strchr(arg, TEMP_DELIM);
+  strncpy(pwm, pwmPointer + sizeof(char), sizeof(pwm));
+
+  pwmPointer = 0;
+  strncpy(temp, arg, sizeof(temp));
+
+  CurveItem result = { atoi(temp), atoi(pwm) };
+  return result;
+}
+
+void parseCurveArg(char *arg) {
+  for (int i = 0; i < MAX_CURVE; i++) { curve[i] = EmptyCurveItem; }
+
+  int index = 0;
+  char *ptr = strtok(arg, ARG_DELIM);
+  while (ptr != NULL) {
+    if (index < MAX_CURVE) {
+      CurveItem item = parseCurveItem(ptr);
+      curve[index] = item;
+    } else {
+      printf("WARN: ignoring curve item %s: too many items!\n", ptr);
+    }
+    ptr = strtok(NULL, ARG_DELIM);
+    index++;
+  }
+
+  printf("Curve: ");
+  for (int i = 0; i < MAX_CURVE; i++) {
+    printf("%d -> %d", curve[i].temp, curve[i].pwm);
+    if (i < MAX_CURVE-1 && curve[i+1].temp != -1) {
+      printf(", ");
+    } else {
+      break;
+    }
+  }
+  printf("\n");
+}
+
+void countDisks() {
+  char buffer[32] = {0};
+  int retval = 0;
+  int i;
+
+  for (i = 0; i < MAX_DISKS; i++) { disks[i][0] = 0; }
+
+  for (i = 0; i < MAX_DISKS; i++) {
+    retval = _Z20Get_Disk_Device_NameiiPc(i, 0x10, buffer);
+    if (retval == 0) {
+      break;
+    }
+
+    double t = 0;
+    retval = Hal_Disk_Get_Temperature(buffer, &t, NULL);
+    if (t == 0.00 || retval != 0) {
+      break;
+    }
+
+    strncpy(disks[i], buffer, MAX_DISK_NAME);
+  }
+
+  printf("Found %d disks: ", i);
+  int diskCount = i;
+  for (i = 0; i < diskCount; i++) {
+    printf("%s ", disks[i]);
+  }
+  printf("\n");
+}
+
+int averageTemp() {
+  int retval;
+  int temps[MAX_DISKS + 2] = {0};
+
+  Hal_Sensor_Get_Thermal_Status(SYSTEMP, &temps[0]);
+  Hal_Sensor_Get_Thermal_Status(CPUTEMP, &temps[1]);
+
+  for (int i = 0; i < MAX_DISKS; i++) {
+    if (disks[i][0] == 0) { break; }
+
+    double t = 0;
+    Hal_Disk_Get_Temperature(disks[i], &t, NULL);
+    temps[i + 2] = (int)t;
+  }
+
+  int sum = 0;
+  int count = 0;
+  for (count = 0; count < MAX_DISKS + 2; count++) {
+    if (temps[count] != 0) {
+      sum += temps[count];
+    } else {
+      break;
+    }
+  }
+  return ((double)sum)/count;
+}
+
+int tempToPwm(int temp) {
+  for (int i = 0; i < MAX_CURVE-1; i++) {
+    if (curve[i].temp <= temp && curve[i+1].temp >= temp) {
+      return curve[i].pwm;
+    }
+  }
+  return -1;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    help(argv[0]);
+    return 1;
+  }
+
+  parseFansArg(argv[1]);
+  parseSleepArg(argv[2]);
+  parseCurveArg(argv[3]);
+  countDisks();
+
+  signal(SIGINT, signalHandler);
+  signal(SIGQUIT, signalHandler);
+  signal(SIGTERM, signalHandler);
+
+  int fan_index = atoi(argv[1]);
+  uint fan_pwm = 0 & 0xFF;
+
+  while (keepRunning) {
+    uint current_pwm = 0;
+    int retval = Hal_Fan_Get_Raw_Value(fan_index, &current_pwm);
+    if (retval != 0) {
+      printf("FAILED: return value is %d\n", retval);
+      continue;
+    }
+
+    if (checkTemp == 0 || checkTemp == CHECK_TEMP_INTERVAL) {
+      int avgTemp = averageTemp();
+      printf("Average system temperature: %d\n", avgTemp);
+      checkTemp = 1;
+      fan_pwm = tempToPwm(avgTemp);
+    }
+
+    if (current_pwm != fan_pwm) {
+      printf("Setting fan %d PWM value to %u\n", fan_index, fan_pwm);
+
+      retval = Hal_Fan_Set_Raw_Value(fan_index, fan_pwm);
+      if (retval != 0) {
+        printf("FAILED: return value is %d\n", retval);
+        continue;
+      }
+    }
+    usleep(sleepTime);
+  }
+  printf("Process exited gracefully\n");
+
+  return 0;
+}
